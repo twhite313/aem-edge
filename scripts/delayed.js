b56797391df1ef0bae4deaa39d8c4757d8f9f1e9
Tim White <tim_white@esri.com>
2024-08-07 16:23:46 -0700

    remove calcite

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,5 +5,3 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-import "@esri/calcite-components/dist/components/calcite-biutton";
-import "@esri/calcite-components/dist/calcite/calcite.css";
