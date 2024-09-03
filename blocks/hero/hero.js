daf2a4d0f01c4304c04c33e757ea030441acfbf7
Tim White <tim_white@esri.com>
2024-09-03 16:34:53 -0700

    Add fancy styles for new FeDX name

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,7 +3,7 @@
 const heroBlock = document.querySelector('.hero');
 const h1Element = heroBlock.querySelector('h1');
 
-// if value of h1Element is "FeDX" then wrap the "DX" in a span tag
-if (h1Element.textContent === 'FeDX') {
-  h1Element.innerHTML = h1Element.textContent.replace('DX', '<span>DX</span>');
+// if value of h1Element starts with "FeDX" then wrap the "DX" in a span tag
+if (h1Element.textContent.startsWith('FeDX')) {
+  h1Element.innerHTML = h1Element.textContent.replace('DX', '<span class="dx">DX</span>');
 }
